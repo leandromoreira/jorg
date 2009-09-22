18f08dbab0d1b1be3e2c6acd80cc7ddd20493741
leandro.rhcp99 <leandro.rhcp99@7d9d523c-a77e-11de-bb10-370f7287a6b7>
2009-09-22 22:58:00 +0000

    initial import of gui to jorg project

---
+++ JOrg/src/jorg/gui/config/ConfiguratorException.java
@@ -0,0 +1,20 @@
+
+package jorg.gui.config;
+
+public class ConfiguratorException extends Exception {
+
+    public ConfiguratorException() {
+    }
+
+    public ConfiguratorException(String msg) {
+        super(msg);
+    }
+
+    public ConfiguratorException(Throwable cause) {
+        super(cause);
+    }
+
+    public ConfiguratorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

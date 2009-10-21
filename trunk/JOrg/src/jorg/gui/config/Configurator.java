1d4c2693aa5e1d7a32c566951ce131cb5cf3e505
leandro.rhcp99 <leandro.rhcp99@7d9d523c-a77e-11de-bb10-370f7287a6b7>
2009-10-21 00:13:28 +0000

    file indexing starting...

--- trunk/JOrg/src/jorg/gui/config/Configurator.java
+++ trunk/JOrg/src/jorg/gui/config/Configurator.java
@@ -5,6 +5,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,8 +15,9 @@ import java.util.logging.Logger;
 public final class Configurator {
     //REMEMBER TO PUT SYSTEM.LINEDIVISOR TO PATHS...
     //config + Ssytem.filesperator ,,,,
-    private final static String SETUP_PATH = "",LANG_PATH="";
-    private final static File setupFile = new File(SETUP_PATH+"setup.properties");
+
+    private final static String SETUP_PATH = "", LANG_PATH = "";
+    private final static File setupFile = new File(SETUP_PATH + "setup.properties");
     private static Properties setup = new Properties(), internationalizator = new Properties();
     private static File iFile;
 
@@ -22,7 +26,7 @@ public final class Configurator {
             FileInputStream in = new FileInputStream(setupFile);
             try {
                 setup.load(in);
-                iFile = new File(LANG_PATH+setup.getProperty("language.file"));
+                iFile = new File(LANG_PATH + setup.getProperty("language.file"));
                 FileInputStream in1 = new FileInputStream(iFile);
                 internationalizator.load(in1);
             } catch (IOException ex) {
@@ -39,7 +43,7 @@ public final class Configurator {
             FileOutputStream out = new FileOutputStream(setupFile);
             setup.store(out, "Setup general configurations.");
         } catch (Exception ex) {
-            throw new ConfiguratorException(getInternationlizedText("configuration.save.error")+ex,ex);
+            throw new ConfiguratorException(getInternationlizedText("configuration.save.error") + ex, ex);
         }
     }
 
@@ -49,5 +53,49 @@ public final class Configurator {
 
     public final static String getInternationlizedText(final String key) {
         return internationalizator.getProperty(key);
+    }
+
+    public final static String[] getPersonalTypes() {
+        return getType("types.personal");
+    }
+
+    public final static String[] getCompressionTypes() {
+        return getType("types.compression");
+    }
+
+    public final static String[] getImageTypes() {
+        return getType("types.image");
+    }
+
+    public final static String[] getDocTypes() {
+        return getType("types.docs");
+    }
+
+    public final static String[] getHtmlTypes() {
+        return getType("types.html");
+    }
+
+    public final static String[] getPictureTypes() {
+        return getType("types.picture");
+    }
+
+    public final static String[] getAudioTypes() {
+        return getType("types.audio");
+    }
+
+    public final static String[] getVideoTypes() {
+        return getType("types.video");
+    }
+
+    private final static String[] getType(String type) {
+        String types = setup.getProperty(type);
+        String[] arrayTypes = types.split(",");
+        List<String> list = new ArrayList<String>();
+        for (String element : arrayTypes) {
+            if (!element.trim().equals("")) {
+                list.add(element.toLowerCase().trim());
+            }
+        }
+        return list.toArray(new String[0]);
     }
 }
--- trunk/JOrg/src/jorg/tests/Tests.java
+++ trunk/JOrg/src/jorg/tests/Tests.java
@@ -1,10 +1,23 @@
 package jorg.tests;
 
-import jorg.gui.config.Configurator;
+import java.util.Set;
+import jorgcore.entity.File;
+import jorgcore.entity.FileManager;
 
 public class Tests {
     public static void main(String[] args) {
-        System.out.println(Configurator.getSetupProprerty("language.file"));
-        System.out.println(Configurator.getInternationlizedText("main.version"));
+        Set<String> m = FileManager.listFilesAt("c:\\tmp\\");
+            for (String item : m) {
+                java.io.File file = new java.io.File(item);
+                File f = new File(file);
+                System.out.println("========================================");
+                System.out.println(f.name);
+                System.out.println(f.extension);
+                System.out.println(f.path);
+                System.out.println(f.size);
+                System.out.println(f.size_in_bytes);
+                System.out.println(f.time_last_modified.toLocaleString());
+                System.out.println("========================================");
+            }
     }
 }

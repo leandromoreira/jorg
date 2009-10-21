c2341c252218e16779143f3c4f1969c51662bf12
leandro.rhcp99 <leandro.rhcp99@7d9d523c-a77e-11de-bb10-370f7287a6b7>
2009-10-21 00:13:58 +0000

    file indexing starting...

--- trunk/JOrgCore/src/jorgcore/entity/File.java
+++ trunk/JOrgCore/src/jorgcore/entity/File.java
@@ -1,5 +1,32 @@
 package jorgcore.entity;
 
+import java.util.Date;
+
 public class File {
 
+    public long id;
+    public String name;
+    public String path;
+    public double size;
+    public long size_in_bytes;
+    public Date time_last_modified;
+    public String extension;
+    public int id_unit;
+
+    public File() {
+    }
+
+    public File(java.io.File file) {
+        name = file.getName();
+        path = file.getParent();
+        size = (file.length() / 1024.d / 1024.d);
+        size_in_bytes = file.length();
+        time_last_modified = new Date(file.lastModified());
+        if (file.getName().lastIndexOf(".") != -1) {
+            String ext = file.getName().substring(file.getName().lastIndexOf(".") + 1, file.getName().length());
+            extension = ext.toLowerCase();
+        }else{
+            extension = "";
+        }
+    }
 }

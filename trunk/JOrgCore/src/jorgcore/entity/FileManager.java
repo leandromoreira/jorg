4640c6434d84706ea7f87b8f4cb7ee2aad19747c
leandro.rhcp99 <leandro.rhcp99@7d9d523c-a77e-11de-bb10-370f7287a6b7>
2009-11-14 23:56:35 +0000

    list of drivers

--- trunk/JOrgCore/src/jorgcore/entity/FileManager.java
+++ trunk/JOrgCore/src/jorgcore/entity/FileManager.java
@@ -4,12 +4,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FileManager {
-private final static String fileSeparator = System.getProperty("file.separator");
+
+    private final static String fileSeparator = System.getProperty("file.separator");
+
     public final static Set<File> listFilesAt(final String path) {
         java.io.File dir = new java.io.File(path);
         String[] children = dir.list();
         Set<File> files = new HashSet<File>();
-        
+
         if (children != null) {
             for (int i = 0; i < children.length; i++) {
                 String filename = children[i];
@@ -21,5 +23,17 @@ private final static String fileSeparator = System.getProperty("file.separator")
             }
         }
         return files;
+    }
+
+    public final static String[] getDrives() {
+        java.io.File[] roots = java.io.File.listRoots();
+        String[] drives = new String[roots.length];
+        int index = 0;
+        for (java.io.File file : roots) {
+            if (!file.getAbsolutePath().equals("A:\\")) {
+                drives[index++] = file.getAbsolutePath();
+            }
+        }
+        return drives;
     }
 }

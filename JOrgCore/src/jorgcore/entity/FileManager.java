ae8fd7f03ef880699146319d277b060ff3d91247
leandro.rhcp99 <leandro.rhcp99@7d9d523c-a77e-11de-bb10-370f7287a6b7>
2009-11-14 23:22:20 +0000

    indexing file module is appearing

--- JOrgCore/src/jorgcore/entity/FileManager.java
+++ JOrgCore/src/jorgcore/entity/FileManager.java
@@ -4,19 +4,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FileManager {
-
-    public final static Set<String> listFilesAt(final String path) {
+private final static String fileSeparator = System.getProperty("file.separator");
+    public final static Set<File> listFilesAt(final String path) {
         java.io.File dir = new java.io.File(path);
         String[] children = dir.list();
-        Set<String> files = new HashSet<String>();
-        String fileSeparator = System.getProperty("file.separator");
+        Set<File> files = new HashSet<File>();
+        
         if (children != null) {
             for (int i = 0; i < children.length; i++) {
                 String filename = children[i];
                 if (new java.io.File(path + fileSeparator + filename).isDirectory()) {
                     files.addAll(listFilesAt(path + fileSeparator + filename));
                 } else {
-                    files.add((path + fileSeparator + filename));
+                    files.add(new File(new java.io.File(path + fileSeparator + filename)));
                 }
             }
         }
